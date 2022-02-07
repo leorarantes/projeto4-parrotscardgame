b4d1a3df236e9fce3ac0675bc5c2ce9f4613a9c9
Leonardo Arantes <leonardorarantes95@gmail.com>
2022-02-07 14:00:00 -0300

    Deliver Version

--- scripts.js
+++ scripts.js
@@ -1,5 +1,9 @@
 const cardsArray = ["images/bobrossparrot.gif", "images/tripletsparrot.gif", "images/revertitparrot.gif", "images/metalparrot.gif", "images/fiestaparrot.gif", "images/explodyparrot.gif", "images/unicornparrot.gif"];
 let check = 0;
+let cardsQuant = 0;
+let clickedCards = 0;
+let movesNumber = 0;
+let alreadyplayed = false;
 
 function comparador() { 
 	return Math.random() - 0.5; 
@@ -8,6 +12,7 @@ function comparador() {
 function cardsNumber() {
     const nCardsAux = prompt("Quantas cartas você deseja? (Números pares de 4 a 14)");
     const nCards = parseInt(nCardsAux);
+    cardsQuant = nCards;
 
     if((nCards > 14) || (nCards < 4) || (nCards%2 != 0)) {
         cardsNumber();
@@ -28,13 +33,26 @@ function renderizeCards(numCards) {
 
     randomizedCards.sort(comparador);
 
-    for(i=0; i<numCards; i++) {
-        addCards.innerHTML += `
-            <div class="card${i+1} card" data-identifier="card" onclick="transition(${i+1})">
-                <div class="face back-face" data-identifier="back-face"><img src="${randomizedCards[i]}"></div>
-                <div class="face front-face" data-identifier="front-face"><img src="images/front.png"></div>
-            </div>
-        `;
+    if(alreadyplayed == false) {
+        for(i=0; i<numCards; i++) {
+            addCards.innerHTML += `
+                <div class="card${i+1} card" data-identifier="card" onclick="transition(${i+1})">
+                    <div class="face back-face" data-identifier="back-face"><img src="${randomizedCards[i]}"></div>
+                    <div class="face front-face" data-identifier="front-face"><img src="images/front.png"></div>
+                </div>
+            `;
+        }
+    } else {
+        movesNumber = 0;
+        addCards.innerHTML = "";
+        for(i=0; i<numCards; i++) {
+            addCards.innerHTML += `
+                <div class="card${i+1} card" data-identifier="card" onclick="transition(${i+1})">
+                    <div class="face back-face" data-identifier="back-face"><img src="${randomizedCards[i]}"></div>
+                    <div class="face front-face" data-identifier="front-face"><img src="images/front.png"></div>
+                </div>
+            `;
+        }
     }
 }
 
@@ -45,9 +63,11 @@ function transition(num) {
     const backFaceAux = document.querySelector(`.card${num} .back-face-transition`);
     let cardSelected = document.querySelector(".selected");
 
-    if((frontFaceAux == null) && (backFaceAux == null)) {
+    if((frontFaceAux == null) && (backFaceAux == null) && (clickedCards < 2)) {
         frontFace.classList.add("front-face-transition");
         backFace.classList.add("back-face-transition");
+        clickedCards++;
+        movesNumber++;
 
         if(cardSelected !== null) {
             cardSelected = document.querySelector(".selected .back-face");
@@ -55,8 +75,15 @@ function transition(num) {
                 cardSelected = document.querySelector(".selected");
                 cardSelected.classList.remove("selected");
                 check++;
+                unclickCards();
+                if(check === (cardsQuant/2)) {
+                    alreadyplayed = true;
+                    check = 0;
+                    setTimeout(winAlert, 500);
+                }
             }
             else {
+                setTimeout(unclickCards, 1000);
                 setTimeout(removeSelected, 1000);
                 cardSelected = document.querySelector(`.card${num}`);
                 cardSelected.classList.add("selected");
@@ -81,4 +108,22 @@ function removeSelected() {
         selected = document.querySelector(".selected");
         selected.classList.remove("selected");
     }
+
+}
+
+function winAlert() {
+    let playAgain = '';
+    playAgain = prompt(`Você ganhou em ${movesNumber} jogadas! Deseja jogar novamente? s/n`);
+
+    if(playAgain == 's') {
+        cardsNumber();
+        playAgain = 'n';
+    }
+    if(playAgain !== 'n') {
+        winAlert();
+    }
+}
+
+function unclickCards() {
+    clickedCards = clickedCards - 2;
 }
