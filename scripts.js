b8200654f26a7db915c2bf3e86ce6c2dbbbeeca8
Leonardo Arantes <leonardorarantes95@gmail.com>
2022-02-06 22:01:41 -0300

    rotate-cards-version

--- scripts.js
+++ scripts.js
@@ -1,4 +1,5 @@
 const cardsArray = ["images/bobrossparrot.gif", "images/tripletsparrot.gif", "images/revertitparrot.gif", "images/metalparrot.gif", "images/fiestaparrot.gif", "images/explodyparrot.gif", "images/unicornparrot.gif"];
+let clickedCards = 0;
 
 function comparador() { 
 	return Math.random() - 0.5; 
@@ -29,9 +30,29 @@ function renderizeCards(numCards) {
 
     for(i=0; i<numCards; i++) {
         addCards.innerHTML += `
-            <div class="card" data-identifier="card">
-                <img src="images/front.png">
+            <div class="card${i+1} card" data-identifier="card" onclick="transition(${i+1})">
+                <div class="face back-face" data-identifier="back-face"><img src="${randomizedCards[i]}"></div>
+                <div class="face front-face" data-identifier="front-face"><img src="images/front.png"></div>
             </div>
         `;
+    }
+}
+
+function transition(num) {
+    const frontFace = document.querySelector(`.card${num} .front-face`);
+    const backFace = document.querySelector(`.card${num} .back-face`);
+    const frontFaceAux = document.querySelector(`.card${num} .front-face-transition`);
+    const backFaceAux = document.querySelector(`.card${num} .back-face-transition`);
+
+    if((frontFaceAux == null) && (backFaceAux == null) && (clickedCards < 2)) {
+        frontFace.classList.add("front-face-transition");
+        backFace.classList.add("back-face-transition");
+        clickedCards++;
+    }
+    
+    if((frontFaceAux !== null) || (backFaceAux !== null)) {
+        frontFace.classList.remove("front-face-transition");
+        backFace.classList.remove("back-face-transition");
+        clickedCards--;
     }
 }
